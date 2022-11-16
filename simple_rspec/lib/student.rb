5e7ca7724fb2fe939fe8dc418032c9c58e275662
Duncan Murchison <duncancmurchison@gmail.com>
2022-11-16 12:54:14 -0500

    macBook

---
+++ simple_rspec/lib/student.rb
@@ -0,0 +1,21 @@
+class Student
+  attr_accessor :name, :grade, :school
+
+  def initialize(name, grade, school)
+    @name = name
+    @grade = grade
+    @school = school
+  end
+
+  def goes_to
+    @name + " goes to " + @school
+  end
+
+end
+
+student1 = Student.new("Duncan", 11, "Huntington High School")
+student2 = Student.new("Bill", 12, "Whitman")
+
+p student1.goes_to
+p student2.goes_to
+
